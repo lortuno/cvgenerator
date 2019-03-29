428ed4a7d6e01ca7110cba695b302dfcaf26a03b
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-03-29 17:58:16 +0100

    Importa bootstrap como es6.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 System.paths['jquery'] = '/node_modules/jquery/dist/jquery.min.js';
+System.paths['bootstrap'] = '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 System.import('/src/services/vue.js');
