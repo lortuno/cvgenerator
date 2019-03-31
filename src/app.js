810924e653e6b8f740fb0fc53bc63f99e6591809
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-03-31 23:38:20 +0200

    Carga jqeury y bootstrap desde index

--- src/app.js
+++ src/app.js
@@ -1,4 +1,2 @@
 System.paths['jquery'] = '/node_modules/jquery/dist/jquery.min.js';
-System.paths['bootstrap'] = '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 System.import('/src/framework/index.js');
-
